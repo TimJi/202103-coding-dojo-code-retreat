83977952ae6f2bd73019d9a26173fc5144e9d87b
Tim Ji <timji@kktv.me>
2021-03-06 14:33:59 +0800

    Finish parseInput driver: fong navigator: tree

--- test/codeRetreat.test.js
+++ test/codeRetreat.test.js
@@ -1,23 +1,34 @@
 import { expect } from "chai"
+import { countBy } from "lodash"
 
 function getPlayers(input) {
+  const player1 = input.split("  ")[0]
+  const player2 = input.split("  ")[1]
+  const player1Name = player1.split(":")[0]
+  const player1Dices = player1
+    .split(":")[1]
+    .split(" ")
+    .map(str => parseInt(str))
+  const player2Name = player2.split(":")[0]
+  const player2Dices = player2
+    .split(":")[1]
+    .split(" ")
+    .map(str => parseInt(str))
+
   return [
     {
-      name: "Amy",
-      dices: [6, 6, 6, 6],
+      name: player1Name,
+      dices: player1Dices,
     },
     {
-      name: "Lin",
-      dices: [6, 6, 6, 6],
+      name: player2Name,
+      dices: player2Dices,
     },
   ]
 }
 
 function getCategory(player) {
-  const _map = player.dices.reduce((obj, val) => {
-    obj[val] = obj[val] ? obj[val] + 1 : 1
-    return obj
-  }, {})
+  const _map = countBy(player.dices)
 
   if (Object.keys(_map).length === 1) {
     return "ALL_THE_SAME_KIND"
@@ -45,6 +56,13 @@ describe("DicesGame", () => {
     let input = {
       name: "Amy",
       dices: [1, 2, 3, 4],
+    }
+    expect(getCategory(input)).to.equal("NO_POINT")
+  })
+  it("should be no point when 3 of them are the same", () => {
+    let input = {
+      name: "Amy",
+      dices: [6, 6, 6, 3],
     }
     expect(getCategory(input)).to.equal("NO_POINT")
   })
