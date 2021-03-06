99aebd5da8428ade216f7568e001991637289c6e
Tim Ji <timji@kktv.me>
2021-03-06 15:23:57 +0800

    sibala wins by all the same kind
    driver: Ralph
    navigator: Fong

--- test/codeRetreat.test.js
+++ test/codeRetreat.test.js
@@ -39,6 +39,18 @@ function getCategory(player) {
   return "NORMAL_POINT"
 }
 
+function sibala(input) {
+  const [player1, player2] = getPlayers(input)
+  if (
+    getCategory(player1) === "ALL_THE_SAME_KIND" &&
+    getCategory(player2) !== "ALL_THE_SAME_KIND"
+  ) {
+    return "Amy wins, all the same kind: 6"
+  }
+
+  throw new Error("not implement")
+}
+
 describe("DicesGame", () => {
   it("Parse Input", () => {
     let input = "Amy:6 6 6 6  Lin:6 6 6 6"
@@ -46,35 +58,48 @@ describe("DicesGame", () => {
     expect(getPlayers(input)[1].name).to.equal("Lin")
     expect(getPlayers(input)[0].dices[0]).to.equal(6)
   })
+  describe("getCategory", () => {
+    it("category: all the same kind", () => {
+      let input = {
+        name: "Amy",
+        dices: [6, 6, 6, 6],
+      }
+      expect(getCategory(input)).to.equal("ALL_THE_SAME_KIND")
+    })
+    it("should be no point", () => {
+      let input = {
+        name: "Amy",
+        dices: [1, 2, 3, 4],
+      }
+      expect(getCategory(input)).to.equal("NO_POINT")
+    })
+    it("should be no point", () => {
+      let input = {
+        name: "Amy",
+        dices: [2, 2, 2, 3],
+      }
+      expect(getCategory(input)).to.equal("NO_POINT")
+    })
 
-  it("category: all the same kind", () => {
-    let input = {
-      name: "Amy",
-      dices: [6, 6, 6, 6],
-    }
-    expect(getCategory(input)).to.equal("ALL_THE_SAME_KIND")
-  })
-  it("should be no point", () => {
-    let input = {
-      name: "Amy",
-      dices: [1, 2, 3, 4],
-    }
-    expect(getCategory(input)).to.equal("NO_POINT")
+    it("should be normal point ", () => {
+      let input = {
+        name: "Amy",
+        dices: [6, 6, 5, 3],
+      }
+      expect(getCategory(input)).to.equal("NORMAL_POINT")
+    })
+
+    it("should be normal point ", () => {
+      let input = {
+        name: "Amy",
+        dices: [6, 6, 2, 2],
+      }
+      expect(getCategory(input)).to.equal("NORMAL_POINT")
+    })
   })
 
-  it("should be normal point ", () => {
-    let input = {
-      name: "Amy",
-      dices: [6, 6, 5, 3],
-    }
-    expect(getCategory(input)).to.equal("NORMAL_POINT")
-  })
-
-  it("should be normal point ", () => {
-    let input = {
-      name: "Amy",
-      dices: [6, 6, 2, 2],
-    }
-    expect(getCategory(input)).to.equal("NORMAL_POINT")
+  it("ALL_THE_SAME_KIND vs. NORMAL_POINT", () => {
+    const input = "Amy:6 6 6 6  Lin:6 6 3 4"
+    expect(sibala(input)).to.equal("Amy wins, all the same kind: 6")
   })
 })
